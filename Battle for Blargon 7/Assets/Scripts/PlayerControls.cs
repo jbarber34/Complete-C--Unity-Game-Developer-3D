a6881a32f5affc0d61ede60a72d032fd056d504a
James Barber <jpbarber34@gmail.com>
2023-12-22 15:08:08 -0500

    Add Input system, both old and new unity versions

--- Battle for Blargon 7/Assets/Scripts/PlayerControls.cs
+++ Battle for Blargon 7/Assets/Scripts/PlayerControls.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+// using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    // NEW INPUT SYSTEM //
+    // [SerializeField] InputAction movement;
+    // [SerializeField] InputAction fire;
+    // NEW INPUT SYSTEM //
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
+    // NEW INPUT SYSTEM //
+    // void OnEnable()
+    // {
+    //     movement.Enable();
+    // }
+
+    // void OnDisable()
+    // {
+    //     movement.Disable();
+    // }
+    // NEW INPUT SYSTEM //
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Old Unity Input System //
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        // Old Unity Input System //
+
+        // New Unity Input System //
+        // float xThrow = movement.ReadValue<Vector2>().x;
+        // float yThrow = movement.ReadValue<Vector2>().y;
+        // New Unity Input System //
     }
 }
