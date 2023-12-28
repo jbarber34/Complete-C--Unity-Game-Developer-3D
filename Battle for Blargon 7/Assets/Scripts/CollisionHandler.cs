bf3ea9aef88b2eb5fed2b10c8ba1f751c3e5f1c3
James Barber <jpbarber34@gmail.com>
2023-12-28 17:48:44 -0500

    Add Reload on Crash; Crash Particles and trigger effects; Hide children mesh's on crash

--- Battle for Blargon 7/Assets/Scripts/CollisionHandler.cs
+++ Battle for Blargon 7/Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other)
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
+
+    [SerializeField] GameObject[] meshChildren;
+
+    void HideMeshInChildren()
     {
-        Debug.Log(this.name + " collided with " + other.gameObject.name);
+        foreach (GameObject part in meshChildren)
+        {
+            // HERE: Get the mesh renderer and box collider from the part
+            MeshRenderer mesh = part.GetComponentInChildren<MeshRenderer>();
+            mesh.enabled = false;
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name} triggered with {other.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Reload the level on collision with anything else
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void StartCrashSequence()
+    {
+        crashVFX.Play();
+        HideMeshInChildren();
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
     }
 }
